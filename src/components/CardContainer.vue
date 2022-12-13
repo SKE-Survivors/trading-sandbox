63d767a025b769bec85c1195e14e5be82dfad106
Nanthakarn Limkool <nanthakarn18@gmail.com>
2022-12-13 13:15:27 +0700

    refactor(CardContainer): loading state

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -2,31 +2,30 @@
 import axios from "axios";
 import Card from "./Card.vue"
 
-async function getPairs() {
-    return await axios.get("http://localhost:3000/pairs")
-}
-
-const totalPair = await getPairs()
-
 export default {
     components: {
         Card
     },
     data() {
         return {
-            pairs: totalPair["data"]
+            pairs: null
         }
-    }
+    },
+    async created() {
+        const totalPair = await axios.get("http://localhost:3000/pairs")
+        this.pairs = totalPair["data"]
+    },
 }
 </script>
+
 <template>
     <div class="col scroller">
         <div class="col" v-for="currency in pairs">
             <Card :tradingCurrencies="currency" :symbol="'◑'" />
         </div>
     </div>
-
 </template>
+
 <style scoped>
 .scroller {
     overflow-x: scroll;
