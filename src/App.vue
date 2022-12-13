34637a7248f037d36992f91ac00ebf52f621074b
Nanthakarn Limkool <nanthakarn18@gmail.com>
2022-12-13 14:42:56 +0700

    feat(init): PriceChart component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <script>
-import Transactor from "./components/Transactor.vue"
-import CardContainer from "./components/CardContainer.vue"
+import Transactor from "./components/Transactor.vue";
+import CardContainer from "./components/CardContainer.vue";
+import PriceChart from "./components/PriceChart.vue";
 
 export default {
   components: {
     CardContainer,
-    Transactor
+    Transactor,
+    PriceChart,
   },
-}
+};
 </script>
 
 <template>
-  <div class="row" style="width: 100%; height: 20%; margin: 0%;">
-    <CardContainer/>
+  <div class="row" style="width: 100%; height: 20%; margin: 0%">
+    <CardContainer />
   </div>
 
   <div class="row" style="width: 100%; margin: 0%">
     <div class="col">
+      <PriceChart />
+    </div>
+    <div class="col">
       <Transactor />
     </div>
   </div>
-
 </template>
 
 <style scoped>
-
 </style>
