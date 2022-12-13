b63658ffa4559c1562548b6e20c50d6af6d0f3ce
Nanthakarn Limkool <nanthakarn18@gmail.com>
2022-12-13 14:35:30 +0700

    fix(CardContainer): smell cade

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -1,34 +1,34 @@
 <script>
 import axios from "axios";
-import Card from "./Card.vue"
+import Card from "@/components/Card.vue";
 
 export default {
-    components: {
-        Card
-    },
-    data() {
-        return {
-            pairs: null
-        }
-    },
-    async created() {
-        const totalPair = await axios.get("http://localhost:3000/pairs")
-        this.pairs = totalPair["data"]
-    },
-}
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      pairs: null,
+    };
+  },
+  async created() {
+    const totalPair = await axios.get("http://localhost:3000/pairs");
+    this.pairs = totalPair["data"];
+  },
+};
 </script>
 
 <template>
-    <div class="col scroller">
-        <div class="col" v-for="currency in pairs">
-            <Card :tradingCurrencies="currency" :symbol="'◑'" />
-        </div>
+  <div class="col scroller">
+    <div class="col" v-for="currency in pairs" :key="currency">
+      <Card :tradingCurrencies="currency" :symbol="'◑'" />
     </div>
+  </div>
 </template>
 
 <style scoped>
 .scroller {
-    overflow-x: scroll;
-    display: flex;
+  overflow-x: scroll;
+  display: flex;
 }
 </style>
