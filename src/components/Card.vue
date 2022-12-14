8d85dd3ebb8e64f9523a5bc01f4dcd7808b6f946
Sahadporn Charnlertlakha <59825486+Sahadporn@users.noreply.github.com>
2022-12-14 17:34:03 +0700

    [.]add % and mid to card/make card load faster

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,35 +1,48 @@
 <script>
+import axios from "axios";
 export default {
-    props: [
-        'tradingCurrencies',
-        'symbol',
-    ],
-    data() {
-        return {
-            displayCurrency: this.tradingCurrencies.replace("-", "/").toUpperCase(),
-            value: 0
-        }
-    },
-    created() {
-        new WebSocket('wss://stream.binance.com:9443/ws/' + this.tradingCurrencies.replace("-", "") + '@miniTicker')
-        .addEventListener('message', e => {
-            let data = JSON.parse(e.data) || {};
-            let { s, c } = data;
-
-            this.value = Number(c).toFixed(2);
-        });
+  props: [
+    'tradingCurrencies',
+    'symbol',
+  ],
+  data() {
+    return {
+      displayCurrency: this.tradingCurrencies.replace("-", "/").toUpperCase(),
+      value: 0,
+      percentChange: 0,
+      midPrice: 0
     }
+  },
+  async created() {
+    const url = "https://api.binance.com/api/v3/ticker/24hr?symbol=";
+    const res = await axios.get(
+      url + this.tradingCurrencies.replace("-", "").toUpperCase()
+      );
+      this.value = Number(res["data"]["lastPrice"]).toFixed(2);
+      this.percentChange = Number(res["data"]["priceChangePercent"]).toFixed(2)
+      this.midPrice = Number(res["data"]["weightedAvgPrice"]).toFixed(2)
+      
+      new WebSocket('wss://stream.binance.com:9443/ws/' + this.tradingCurrencies.replace("-", "") + '@ticker')
+        .addEventListener('message', e => {
+          let data = JSON.parse(e.data) || {};
+          let { c, P, w } = data;
+  
+          this.value = Number(c).toFixed(2);
+          this.percentChange = Number(P).toFixed(2);
+          this.midPrice = Number(w).toFixed(2);
+        });
+  }
 }
 </script>
 
 <template>
   <div id="card">
-    <font-awesome-icon
-      icon="fa-solid fa-circle-half-stroke"
-      style="height: 2em; width: 2em"
-    />
+    <font-awesome-icon icon="fa-solid fa-circle-half-stroke" style="height: 2em; width: 2em" />
     <h1>{{ displayCurrency }}</h1>
     <h2>{{ symbol }} {{ value }}</h2>
+    <h2> % {{ percentChange }}</h2>
+    <h2> mid price </h2>
+    <h2> {{ midPrice }} </h2>
   </div>
 </template>
 
