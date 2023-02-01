a9f10fa21aeaa87abe6da156b4518fac8e996ea4
Nanthakarn Limkool <nanthakarn18@gmail.com>
2023-02-01 11:44:50 +0700

    deploy(setup): setup deployment

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:19-alpine
+
+WORKDIR /app
+COPY . .
+
+EXPOSE 3000
+
+RUN npm install
+
+CMD ["npm", "run", "dev"]
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: './',
-})
+  // ! note
+  console.log("env", env);
+  console.log("process.env", process.env);
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    base: "./",
+    server: {
+      host: "0.0.0.0",
+      port: parseInt(process.env.PORT) || parseInt(env.VITE_PORT) || 5173,
+    },
+  };
+});
