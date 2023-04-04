3510be3a52361596cc44dd43906d9eac92def276
Nanthakarn Limkool <nanthakarn18@gmail.com>
2023-04-05 02:55:41 +0700

    feat: read order book from ws

--- src/components/DepthChart.vue
+++ src/components/DepthChart.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="chart" id="depthchart">This is Depth Chart for {{ symbol }}</div>
+  <div class="chart" id="depthchart">
+    This is Depth Chart for {{ symbol }}:
+    {{ d }}
+  </div>
 </template>
 
 <script>
-function loadChart(symbol) {
-  // do something
-}
-
 export default {
   props: ["symbol"],
+  data() {
+    return {
+      ws: null,
+      d: null,
+    };
+  },
   mounted() {
-    loadChart(this.symbol);
+    this.loadChart(this.symbol);
   },
   watch: {
     symbol() {
-      loadChart(this.symbol);
+      this.loadChart(this.symbol);
+    },
+  },
+  methods: {
+    updateData(e) {
+      let data = JSON.parse(e.data) || {};
+      // console.log(data);
+      this.d = data;
+    },
+    async loadChart(symbol) {
+      if (this.ws != null) {
+        this.ws.removeEventListener("message", this.updateData);
+      }
+      this.ws = new WebSocket(
+        "wss://stream.binance.com:9443/ws/" + symbol.toLowerCase() + "@depth20"
+      );
+      this.ws.addEventListener("message", this.updateData);
     },
   },
 };
