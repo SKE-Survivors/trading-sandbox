f3be17c70e034030977b5c6e08cab82d35813887
Nanthakarn Limkool <nanthakarn18@gmail.com>
2023-04-10 19:31:24 +0700

    feat(DepthChart): add UI for DepthChart data

--- src/components/DepthChart.vue
+++ src/components/DepthChart.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="chart" id="depthchart">
-    This is Depth Chart for {{ symbol }}:
-    {{ d }}
-  </div>
+  <div class="chart" id="depthchart"></div>
 </template>
 
 <script>
@@ -11,7 +8,6 @@ export default {
   data() {
     return {
       ws: null,
-      d: null,
     };
   },
   mounted() {
@@ -26,7 +22,49 @@ export default {
     updateData(e) {
       let data = JSON.parse(e.data) || {};
       // console.log(data);
-      this.d = data;
+
+      let asksData = data.asks.map((v) => [parseFloat(v[0]), parseFloat(v[1])]);
+      let bidsData = data.bids.map((v) => [parseFloat(v[0]), parseFloat(v[1])]);
+      let midmarket = (asksData[0][0] + bidsData[0][0]) / 2;
+
+      zingchart.exec("depthchart", "setdata", {
+        data: {
+          "background-color": "#171b26",
+          type: "depth",
+          options: {
+            midMarketPrice: midmarket,
+            mmpMarker: {
+              alpha: 0.75,
+              // lineColor: "#999",
+              label: {
+                color: "#fff",
+                fontSize: 12,
+                offsetY: 20,
+              },
+            },
+            // currency: "$",
+            palette: ["#f13645", "#019a81"],
+            title: {
+              color: "#fff",
+              // text: "title",
+            },
+            subtitle: {
+              visible: true,
+              text: this.symbol.toUpperCase(),
+            },
+          },
+          series: [
+            {
+              values: asksData,
+              text: "Asks",
+            },
+            {
+              values: bidsData,
+              text: "Bids",
+            },
+          ],
+        },
+      });
     },
     async loadChart(symbol) {
       if (this.ws != null) {
@@ -36,6 +74,26 @@ export default {
         "wss://stream.binance.com:9443/ws/" + symbol.toLowerCase() + "@depth20"
       );
       this.ws.addEventListener("message", this.updateData);
+
+      zingchart.exec("depthchart", "destroy");
+      zingchart.render({
+        id: "depthchart",
+        height: "100%",
+        data: {
+          "background-color": "#171b26",
+          type: "depth",
+          options: {
+            title: {
+              color: "#fff",
+              text: " Loading... ",
+            },
+            subtitle: {
+              visible: true,
+              text: this.symbol.toUpperCase(),
+            },
+          },
+        },
+      });
     },
   },
 };
@@ -43,7 +101,7 @@ export default {
 
 <style scoped>
 .chart {
-  height: 80%;
-  min-height: 75vh;
+  height: 80% !important;
+  min-height: 75vh !important;
 }
 </style>
